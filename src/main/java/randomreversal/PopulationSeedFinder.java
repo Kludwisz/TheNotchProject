0cd98ba0b13e504af03e143228a7a1fda3efac43
Kludwisz <kludwisz@gmail.com>
2025-04-15 19:45:10 +0200

    get rid of 128-bit integers, implement algo

--- src/main/java/randomreversal/PopulationSeedFinder.java
+++ src/main/java/randomreversal/PopulationSeedFinder.java
@@ -1,5 +1,7 @@
 package randomreversal;
 
+import com.seedfinding.mccore.util.pos.CPos;
+
 import java.math.BigInteger;
 
 public class PopulationSeedFinder {
@@ -24,59 +26,32 @@ public class PopulationSeedFinder {
     }
 
     private static void lagrange_gauss(long[][] basis) {
-        // TODO implement this function
+        int swaps = 0;
+        while (true) {
+            int q = Math.round(dot(basis[0][0], basis[0][1], basis[1][0], basis[1][1]) / lensq(basis[1][0], basis[1][1]));
+            if (q == 0) break;
+            basis[0][0] -= q * basis[1][0];
+            basis[0][1] -= q * basis[1][1];
+            swaps++;
+            swapRows(basis);
+        }
+
+        if (swaps % 2 == 1) {
+            swapRows(basis);
+        }
     }
 
-    /*
-    uint64_t b_inv = modinv64(b) & MASK_60;
-    // std::println("b_inv = {}", b_inv);
-
-    // a * x + b * z = t
-
-    int64_t basis[2][2] = {
-        { 0, MOD_60 },
-        { 1, (int64_t)(-a * b_inv & MASK_60) },
-    };
-    // std::println("basis =\n{} {}\n{} {}", basis[0][0], basis[0][1], basis[1][0], basis[1][1]);
-
-    lagrange_gauss(basis);
-    // std::println("reduced basis =\n{} {}\n{} {}", basis[0][0], basis[0][1], basis[1][0], basis[1][1]);
-
-    uint64_t basis_det = basis[0][0] * basis[1][1] - basis[0][1] * basis[1][0];
-    if (basis_det != MOD_60) throw 0;
-    // std::println("basis_det = {}", basis_det);
-
-    int64_t basis_inv_num[2][2] = {
-        { basis[1][1], -basis[0][1] },
-        { -basis[1][0], basis[0][0] },
-    };
-
-    int64_t new_z_center = b_inv * target << 4 >> 4;
-    // std::println("new_z_center = {}", new_z_center);
-
-    int64_t lx = ((__int128)basis_inv_num[1][0] * -new_z_center + (basis_det >> 1)) >> 60;
-    int64_t lz = ((__int128)basis_inv_num[1][1] * -new_z_center + (basis_det >> 1)) >> 60;
-    // std::println("lx = {} lz = {}", lx, lz);
-
-    __int128 bx = (__int128)lx * basis[0][0] + (__int128)lz * basis[1][0];
-    __int128 bz = (__int128)lx * basis[0][1] + (__int128)lz * basis[1][1] + new_z_center;
-    if (bx != bx << 64 >> 64 || bz != bz << 64 >> 64) throw 0;
-    // std::println("x = {} z = {}", (int64_t)x, (int64_t)z);
-
-    int64_t x = bx;
-    int64_t z = bz;
-
-    uint64_t result = (a * x + b * z) & MASK_60;
-    if (result != target) throw 0;
-    // std::println("result = {} target = {}", result, target & MASK_60);
-
-    if (x >= min_x && x <= max_x && z >= min_z && z <= max_z) {
-        out.push_back({ x, z });
+    private static void swapRows(long[][] basis) {
+        long temp = basis[0][0];
+        basis[0][0] = basis[1][0];
+        basis[1][0] = temp;
+        temp = basis[0][1];
+        basis[0][1] = basis[1][1];
+        basis[1][1] = temp;
     }
 
-     */
-    public static Candidate findSolutionInBox(long a, long b, long target) {
-        // TODO andrew's & scriptline's algo goes here
+
+    public static CPos findSolutionInBox(long a, long b, long target) {
         final int minX = -30_000_000 / 16;
         final int maxX = 30_000_000 / 16;
         final int minZ = -30_000_000 / 16;
@@ -100,8 +75,31 @@ public class PopulationSeedFinder {
 
         long new_z_center = b_inv * target << 4 >> 4;
 
+        long lx = BigInteger.valueOf(basis_inv_num[1][0])
+                .multiply(BigInteger.valueOf(-new_z_center))
+                .add(BigInteger.valueOf(basis_det >> 1))
+                .shiftRight(60).longValue();
+        long lz = BigInteger.valueOf(basis_inv_num[1][1])
+                .multiply(BigInteger.valueOf(-new_z_center))
+                .add(BigInteger.valueOf(basis_det >> 1))
+                .shiftRight(60).longValue();
 
+        long x = BigInteger.valueOf(lx)
+                .multiply(BigInteger.valueOf(basis[0][0]))
+                .add(BigInteger.valueOf(lz).multiply(BigInteger.valueOf(basis[1][0])))
+                .longValue();
+        long z = BigInteger.valueOf(lx)
+                .multiply(BigInteger.valueOf(basis[0][1]))
+                .add(BigInteger.valueOf(lz).multiply(BigInteger.valueOf(basis[1][1])))
+                .add(BigInteger.valueOf(new_z_center))
+                .longValue();
 
+        long result = (a * x + b * z) & MASK_60;
+        if (result != target) return null;
+
+        if (x >= minX && x <= maxX && z >= minZ && z <= maxZ) {
+            return new CPos((int)x, (int)z);
+        }
         return null;
     }
 }
